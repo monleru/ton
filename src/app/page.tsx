792a90a07c293abcc2de28177063380922eac224
monleru <kucherenko.alexandr1998@gmail.com>
2024-05-29 21:43:02 +0300

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,9 @@ import Image from "next/image";
 import { useState } from "react";
 
 export default function Home() {
-  const [clicks,setClicks] = useState<any[]>([])
+  const [clicks,setClicks] = useState<number>(0)
   const click = (event:any) => {
+    setClicks(num => num + 1)
     const parentElement = document.getElementById('lol');
     const newElement = document.createElement('div');
     newElement.className = 'number';
@@ -24,8 +25,10 @@ export default function Home() {
   }
   return (
     <main id="lol" className="flex min-h-screen flex-col items-center justify-between p-24">
-      <button onClick={click} className="w-48 h-48 flex items-center justify-center text-center bg-red-500 rounded-full">Click me</button>
-      <div></div>
+      <button onClick={click} className="active:scale-95 transition w-48 h-48 flex items-center justify-center text-center bg-red-500 rounded-full">Click me</button>
+      <div>
+        points {clicks}
+      </div>
     </main>
   );
 }
